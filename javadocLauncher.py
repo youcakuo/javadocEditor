8cc87025fa6047bc98a4794654491b71c91de553
Shen Kuo <shen.kuo@iisigroup.com>
2019-08-05 15:24:58 +0800

    minor fix for launcher

--- javadocLauncher.py
+++ javadocLauncher.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[11]:
 
 
 import os, subprocess
 import shutil
+import time, sys
 
 try:
     shutil.copy('\\\\10.204.1.80\\tfb\\T-NBTS 專案_第二階段\\工作區\個人工作區\\Shen\\javadoc\\JavadocEditor.exe', '.\JavadocEditor.exe')
@@ -14,11 +15,10 @@ except:
 finally:
     print(os.getcwd())
     os.system('JavadocEditor.exe')
-    #winView = './JavadocEditor.exe'
-    #subprocess.run(winView, shell=True)
-    print('done...')
-
-    
+    print('done', end='', flush=True)
+    for i in range(3):
+        time.sleep(0.7)
+        print('.', end='', flush = True)
 
 
 # In[ ]:
